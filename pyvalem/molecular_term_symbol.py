ddf8f02d231adebc4d020688f75f4ef403ebb5c1
Mayya Bondarevskaya <mayya.bondarevskaya@gmail.com>
2016-11-03 12:17:46 +0000

    Updated orbital_irrep_labels to contain all possible point group labels

--- pyvalem/molecular_term_symbol.py
+++ pyvalem/molecular_term_symbol.py
@@ -12,18 +12,25 @@ import pyparsing as pp
 from .state import State, StateParseError
 
 orbital_irrep_labels = (
-    'Σ-', 'Σ+', 'Π', 'Δ',
-    'Σ-u', 'Σ+u', 'Πu', 'Δu',
-    'Σ-g', 'Σ+g', 'Πg', 'Δg',
-    'A', "A'", 'A"', 'A1', 'A2', 'A3',
+    'Σ-', 'Σ-u', 'Σ-g',
+    'Σ+', 'Σ+u', 'Σ+g', 
+    'Π', 'Πu', 'Πg', 
+    'Δ','Δu','Δg',
+    'Φ', 'Φg', 'Φu',
+    'A', "A'", 'A"', 'A1', "A'1", 'A"1', 'A2', "A'2", 'A"2', 'A3',
     'Ag', "A'g", 'A"g', 'A1g', 'A2g', 'A3g',
     'Au', "A'u", 'A"u', 'A1u', 'A2u', 'A3u',
     'B', "B'", 'B"', 'B1', 'B2', 'B3',
     'Bg', "B'g", 'B"g', 'B1g', 'B2g', 'B3g',
     'Bu', "B'u", 'B"u', 'B1u', 'B2u', 'B3u',
-    'E', "E'", 'E"', 'E1', 'E2',
-    'Eg', "E'g", 'E"g', 'E1g', 'E2g',
-    'Eu', "E'u", 'E"u', 'E1u', 'E2u',
+    'E', "E'", 'E"', 'E1', "E'1", 'E"1', 'E2', "E'2", 'E"2', 'E3', "E'3", 'E"3', 'E4', 'E5', 'E6', 'E7',
+    'Eg', "E'g", 'E"g', 'E1g', 'E2g', 'E3g',
+    'Eu', "E'u", 'E"u', 'E1u', 'E2u', 'E3u',
+    'T', 'T1', 'T2',
+    'Tg', 'T1g', 'T2g',
+    'Tu', 'T1u', 'T2u',
+    'G', 'Gg', 'Gu',
+    'H', 'Hg', 'Hu'
 )
 
 integer = pp.Word(pp.nums)
