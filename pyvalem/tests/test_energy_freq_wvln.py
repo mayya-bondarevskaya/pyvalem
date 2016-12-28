77ad097189b7e5f2fd5a50080a8262a436590ef9
Mayya Bondarevskaya <mayya.bondarevskaya@gmail.com>
2016-12-28 13:00:59 +0000

    added new tests

--- pyvalem/tests/test_energy_freq_wvln.py
+++ pyvalem/tests/test_energy_freq_wvln.py
@@ -6,7 +6,13 @@ class EnergyFreqWvlnTest(unittest.TestCase):
     def test_energy_input(self):
         e1 = EnergyFreqWvln('200 J')
         e1_energy = e1.energy()
+        e1_energy_ev = e1.energy(units = 'eV')
+        e1_freq = e1.frequency()
+        e1_wvln = e1.wavelength()
         self.assertEqual(e1_energy.value,200)
+        self.assertTrue(abs(e1_energy_ev.value-1.2483019294240832e+21) < 0.01)
+        self.assertTrue(abs(e1_freq.value-3.018380901366291e+35) < 0.01)
+        self.assertTrue(abs(e1_wvln.value-3.9728910007918073e-23) < 0.01)
         
     def test_frequency_input(self):
         pass
