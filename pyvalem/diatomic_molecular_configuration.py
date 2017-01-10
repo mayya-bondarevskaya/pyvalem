bf4e7929dd9736b2db09aa83060460e73e959db7
Mayya Bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-01-10 13:29:25 +0000

    started testing of diatomic molecular configuration class. currently running into issues that optional parameters are not being read as optional, ie 1SIGMA1 parses, but 1SIGMA doesnt

--- pyvalem/diatomic_molecular_configuration.py
+++ pyvalem/diatomic_molecular_configuration.py
@@ -15,11 +15,12 @@ greek_letters = {'SIGMA': 'σ',
                  'SIGMAg':'σg',
                  'PIu':'πu'}
 molecule_orbital = pp.Group(integer.setResultsName('n')
-                            +pp.oneOf(molecular_orbital_symbols).setResultsName('symbol'))
+                            +pp.oneOf(molecular_orbital_symbols).setResultsName('symbol')
+                            +pp.Optional(integer.setResultsName('count')))
 
-molecule_config = (molecule_orbital + pp.Optional(integer.setResultsName('count')) + 
+molecule_config = (molecule_orbital + 
                    pp.ZeroOrMore(pp.Suppress('.') +
-                                 molecule_orbital + pp.Optional(integer) +
+                                 molecule_orbital + 
                                  pp.StringEnd())
                    ).leaveWhitespace()
                    
@@ -27,8 +28,19 @@ class DiatomicMolecularConfigurationError(StateParseError):
     pass
 
 class MolecularOrbital:
-    def __init__(self, n, symbol, count):
-        self.n = n;
+    def __init__(self, n, symbol, count=1):
+        self.n = n
+        if symbol in greek_letters.keys():
+            self.symbol = greek_letters[symbol]
+        else:
+            self.symbol = symbol
+        self.count = count
+        
+    def __str__(self):
+        pass
+    
+    def validate_molecular_orbital(self):
+        pass
 
 class DiatomicMolecularConfiguration(State):
     def parse_state(self, state_str):
@@ -40,6 +52,24 @@ class DiatomicMolecularConfiguration(State):
             raise StateParseError('Invalid diatomic molecular electronic '
                                   'configuration syntax: {0}'.format(state_str))
         
-        self.orbital = []
+        self.orbitals = []
         for i, parsed_orbital in enumerate(parse_results):
-            temp_oribital = MolecularOrbital()
+            temp_orbital = MolecularOrbital(n = parsed_orbital['n'],
+                                             symbol = parsed_orbital['symbol'],
+                                             count = parsed_orbital['count'])
+            self.orbitals.append(temp_orbital)
+        
+        if len(self.orbitals) != len(set(self.orbitals)):
+            raise DiatomicMolecularConfigurationError('Repeated orbitals in {0}'.format(self.state_str))
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
