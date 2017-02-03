a79a0fa94301d53ffd343875968344ccc54e0472
Mayya Bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-02-03 12:57:46 +0000

    added html tests for DiatomicMolecularConfiguration

--- pyvalem/tests/test_diatomic_molecular_configurations.py
+++ pyvalem/tests/test_diatomic_molecular_configurations.py
@@ -16,13 +16,25 @@ class DiatomicMolecularConfigurationTest(unittest.TestCase):
 
     def test_diatomic_molecular_configuration(self):
         c1 = DiatomicMolecularConfiguration('1σ')
+        self.assertEqual(c1.html, '1σ<sub>1<\sub>')
+        
         c2 = DiatomicMolecularConfiguration('1σ2')
+        self.assertEqual(c2.html, '1σ<sub>2<\sub>')
+        
         c3 = DiatomicMolecularConfiguration('1σg2')
+        self.assertEqual(c3.html, '1σg<sub>2<\sub>')
+        
         c4 = DiatomicMolecularConfiguration('1σu2')
+        self.assertEqual(c4.html, '1σu<sub>2<\sub>')
+        
         c5 = DiatomicMolecularConfiguration('1sigmau')
+        self.assertEqual(c5.html, '1σu<sub>1<\sub>')
+        
         c6 = DiatomicMolecularConfiguration('1sigmau2')
+        self.assertEqual(c6.html, '1σu<sub>2<\sub>')
 
         c7 = DiatomicMolecularConfiguration('1sigmau2.2sigmag1')
+        self.assertEqual(c7.html, '1σu<sub>2<\sub>.2σg<sub>1<\sub>')
 
         self.assertRaises(StateParseError, DiatomicMolecularConfiguration,
                           's4.w2')
