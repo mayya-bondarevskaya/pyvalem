93264f95e16772cec45966b57d15b2835c22e33a
Christian Hill <xn.hill@gmail.com>
2017-02-03 13:29:08 +0000

    Replace assertTrue with assertAlmostEqual

--- pyvalem/energy_freq_wvln.py
+++ pyvalem/energy_freq_wvln.py
@@ -1,7 +1,6 @@
 from .state import State, StateParseError
 from pyqn.pyqn.quantity import Quantity
 from pyqn.pyqn.units import Units
-from pyqn.pyqn.dimensions import Dimensions, d_energy, d_length, d_frequency, d_quantity
 #This module HAS to be run from above the main Pyvalem folder, such that
 #this module can also "see" the pyqn directory
 
@@ -9,16 +8,17 @@ class EnergyFreqWvln(State):
     def parse_state(self,state_str):
         self.state_str = state_str
         state_str_split = state_str.split()
-        self.quantity = Quantity(value = float(state_str_split[0]),units = state_str_split[1])
+        self.quantity = Quantity(value=float(state_str_split[0]),
+                                 units = state_str_split[1])
         
-    def energy(self,units='J'):
-        return self.quantity.convert_units_to(units,force='spec')
+    def energy(self, units='J'):
+        return self.quantity.convert_units_to(units, force='spec')
     
-    def frequency(self,units='s-1'):
-        return self.quantity.convert_units_to(units,force='spec')
+    def frequency(self, units='s-1'):
+        return self.quantity.convert_units_to(units, force='spec')
     
-    def wavelength(self,units='m'):
-        return self.quantity.convert_units_to(units,force='spec')
+    def wavelength(self, units='m'):
+        return self.quantity.convert_units_to(units, force='spec')
     
     @property
     def html(self):
