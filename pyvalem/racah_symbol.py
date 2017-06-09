5461f57b3fb53c7b961bfe4f2ae5c23de25f5780
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-06-09 13:16:30 +0100

    fix syntax errors

--- pyvalem/racah_symbol.py
+++ pyvalem/racah_symbol.py
@@ -5,19 +5,16 @@ integer = pp.Word(pp.nums)
 atom_principal = integer.setResultsName('principal')
 
 orbital_labels = ('s', "s'", 
-				  'p', "p'",
-				  'd', "d'",
-				  'f', "f'")
-atom_orbital = pp.OneOf(orbital_labels).setResultsName('orbital')
+                  'p', "p'",
+                  'd', "d'",
+                  'f', "f'")
+atom_orbital = pp.oneOf(orbital_labels).setResultsName('orbital')
 
-atom_k_term = integer.setResultsName('k_num') + pp.Suppress('/') + 
-				integer.setResultsName('k_den')
+atom_k_term = integer.setResultsName('k_num') + pp.Suppress('/') + integer.setResultsName('k_den')
 
 atom_j_term = integer.setResultsName('jterm')
 
-racah_symbol_template = atom_principal + atom_orbital + 
-						pp.Suppress('[') + atom_k_term + 
-						pp.Suppress(']_') + atom_j_term
+racah_symbol_template = atom_principal + atom_orbital + pp.Suppress('[') + atom_k_term + pp.Suppress(']_') + atom_j_term
 
 class RacahSymbol(State):
     """The RacahSymbol class representing an atomic state
@@ -28,24 +25,24 @@ class RacahSymbol(State):
 
     def parse_state(self, state_str):
         try:
-			components = racah_symbol_template(state_str)
-		except pp.PraseException:
-			raise StateParseError('Invalid Racah notation syntax: {:}'
-								.format(state_str))
-		self.principal = int(components.principal)
-		self.orbital = components.orbital
-		if "'" in self.orbital:
-			self.parent_rot = 0.5
-		else:
-			self.parent_rot = 3.2
-		self.k_num = int(components.k_num)
-		self.k_den = int(compoennts.k_den)
-		self.j_term = int(components.jterm)
-		
-	@property
-	def html(self):
-		html_chunks = []
-		html_chunks.append('{:}{:}'.format(self.principal,self.orbital))
-		html_chunks.append('[{:}/{:}]'.format(self.k_num, self.k_den))
-		html_chunks.append('<sub>{:}</sub>'.format(self.j_term))
-		return ''.join(html_chunks)
+            components = racah_symbol_template(state_str)
+        except pp.PraseException:
+            raise StateParseError('Invalid Racah notation syntax: {:}'
+                                .format(state_str))
+        self.principal = int(components.principal)
+        self.orbital = components.orbital
+        if "'" in self.orbital:
+            self.parent_rot = 0.5
+        else:
+            self.parent_rot = 3.2
+        self.k_num = int(components.k_num)
+        self.k_den = int(compoennts.k_den)
+        self.j_term = int(components.jterm)
+        
+    @property
+    def html(self):
+        html_chunks = []
+        html_chunks.append('{:}{:}'.format(self.principal,self.orbital))
+        html_chunks.append('[{:}/{:}]'.format(self.k_num, self.k_den))
+        html_chunks.append('<sub>{:}</sub>'.format(self.j_term))
+        return ''.join(html_chunks)
