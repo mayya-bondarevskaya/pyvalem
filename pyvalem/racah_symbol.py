c83afdd53ec7f3016c3f5eb0e2be0c3fc1a231f7
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-06-09 13:02:24 +0100

    pyparsing template for racah notation

--- pyvalem/racah_symbol.py
+++ pyvalem/racah_symbol.py
@@ -1,6 +1,24 @@
 import pyparsing as pp
 from .state import State, StateParseError
 
+integer = pp.Word(pp.nums)
+atom_principal = integer.setResultsName('principal')
+
+orbital_labels = ('s', "s'", 
+				  'p', "p'",
+				  'd', "d'",
+				  'f', "f'")
+atom_orbital = pp.OneOf(orbital_labels).setResultsName('orbital')
+
+atom_k_term = integer.setResultsName('k_num') + pp.Suppress('/') + 
+				integer.setResultsName('k_den')
+
+atom_j_term = integer.setResultsName('jterm')
+
+racah_symbol_template = atom_principal + atom_orbital + 
+						pp.Suppress('[') + atom_k_term + 
+						pp.Suppress(']_') + atom_j_term
+
 class RacahSymbol(State):
     """The RacahSymbol class representing an atomic state
 
