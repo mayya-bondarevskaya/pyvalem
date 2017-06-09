b1d5e41cab3e4f644798198385db9c4838eb75d2
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-06-09 13:13:22 +0100

    basic racah notation class complete

--- pyvalem/racah_symbol.py
+++ pyvalem/racah_symbol.py
@@ -27,5 +27,25 @@ class RacahSymbol(State):
     """
 
     def parse_state(self, state_str):
-        # nothing to see here, for now... Mayya to write this?
-        pass
+        try:
+			components = racah_symbol_template(state_str)
+		except pp.PraseException:
+			raise StateParseError('Invalid Racah notation syntax: {:}'
+								.format(state_str))
+		self.principal = int(components.principal)
+		self.orbital = components.orbital
+		if "'" in self.orbital:
+			self.parent_rot = 0.5
+		else:
+			self.parent_rot = 3.2
+		self.k_num = int(components.k_num)
+		self.k_den = int(compoennts.k_den)
+		self.j_term = int(components.jterm)
+		
+	@property
+	def html(self):
+		html_chunks = []
+		html_chunks.append('{:}{:}'.format(self.principal,self.orbital))
+		html_chunks.append('[{:}/{:}]'.format(self.k_num, self.k_den))
+		html_chunks.append('<sub>{:}</sub>'.format(self.j_term))
+		return ''.join(html_chunks)
