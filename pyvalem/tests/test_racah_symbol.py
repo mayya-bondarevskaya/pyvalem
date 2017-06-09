3d448022cc60a285d971dcf28bfafaf188b7e4b8
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-06-09 13:37:30 +0100

    add unit tests for racah symbol

--- pyvalem/tests/test_racah_symbol.py
+++ pyvalem/tests/test_racah_symbol.py
@@ -12,19 +12,42 @@ from ..racah_symbol import RacahSymbol
 
 class RacahSymbolTest(unittest.TestCase):
     def test_racah_symbol(self):
+        rs1 = RacahSymbol('4s[3/2]_2')
+        self.assertEqual(rs1.principal, 4)
+        self.assertEqual(rs1.orbital, 's')
+        self.assertEqual(rs1.parent_rot, 1.5)
+        self.assertEqual(rs1.k_num, 3)
+        self.assertEqual(rs1.k_den, 2)
+        self.assertEqual(rs1.j_term, 2)
+        self.assertEqual(rs1.html, '4s[3/2]<sub>2</sub>')
 
-        # The RacahSymbol class is just a stub at the moment, so
-        # it doesn't extend the functionality of the base State class at all.
-        rs1 = RacahSymbol('Absolutely anything at all')
-        self.assertEqual(str(rs1), 'Absolutely anything at all')
-        self.assertEqual(rs1.html, 'Absolutely anything at all')
-
-        # When it has been written, this sort of thing is what a RacahSymbol
-        # looks like.
-        rs2 = RacahSymbol('3d[3/2]_2')
-        self.assertEqual(str(rs2), '3d[3/2]_2')
-        self.assertEqual(rs2.html, '3d[3/2]_2')
-
+        rs2 = RacahSymbol("4p'[1/2]_0")
+        self.assertEqual(rs2.principal, 4)
+        self.assertEqual(rs2.orbital, "p'")
+        self.assertEqual(rs2.parent_rot, 0.5)
+        self.assertEqual(rs2.k_num, 1)
+        self.assertEqual(rs2.k_den, 2)
+        self.assertEqual(rs2.j_term, 0)
+        self.assertEqual(rs2.html, "4p'[1/2]<sub>0</sub>")
+        
+        rs3 = RacahSymbol("3d'[5/2]_3")
+        self.assertEqual(rs3.principal, 3)
+        self.assertEqual(rs3.orbital, "d'")
+        self.assertEqual(rs3.parent_rot, 0.5)
+        self.assertEqual(rs3.k_num, 5)
+        self.assertEqual(rs3.k_den, 2)
+        self.assertEqual(rs3.j_term, 3)
+        self.assertEqual(rs3.html, "3d'[5/2]<sub>3</sub>")
+        
+        rs4 = RacahSymbol("5s'[1/2]_1")
+        self.assertEqual(rs4.principal, 5)
+        self.assertEqual(rs4.orbital, "s'")
+        self.assertEqual(rs4.parent_rot, 0.5)
+        self.assertEqual(rs4.k_num, 1)
+        self.assertEqual(rs4.k_den, 2)
+        self.assertEqual(rs4.j_term, 1)
+        self.assertEqual(rs4.html, "5s'[1/2]<sub>1</sub>")
+        
 if __name__ == '__main__':
     unittest.main()
 
