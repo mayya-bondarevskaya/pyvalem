92d9c13d9fc511af0bfbd730b66a5cc4655bd48b
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-08-09 14:52:27 +0100

    add usage of the quantity classmethod for parsing the string at initialisation

--- pyvalem/energy_freq_wvln.py
+++ pyvalem/energy_freq_wvln.py
@@ -1,15 +1,12 @@
 from .state import State, StateParseError
-from pyqn.pyqn.quantity import Quantity
-from pyqn.pyqn.units import Units
+from pyqn.quantity import Quantity
+from pyqn.units import Units
 #This module HAS to be run from above the main Pyvalem folder, such that
 #this module can also "see" the pyqn directory
 
 class EnergyFreqWvln(State):
     def parse_state(self,state_str):
-        self.state_str = state_str
-        state_str_split = state_str.split()
-        self.quantity = Quantity(value=float(state_str_split[0]),
-                                 units = state_str_split[1])
+        self.quantity = Quantity.parse(state_str)
         
     def energy(self, units='J'):
         return self.quantity.convert_units_to(units, force='spec')
